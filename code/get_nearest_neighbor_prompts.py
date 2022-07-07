ec1e53987238d8f89fd04242d82da8bbb174329c
ncarraz <carraznathanael@live.fr>
2022-07-07 17:31:46 +0200

    add nearest neighbor

--- code/get_nearest_neighbor_prompts.py
+++ code/get_nearest_neighbor_prompts.py
@@ -16,9 +16,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
     model = build_model_by_name(args)
     model.update_embeddings()
+    model.tokenizer.add_tokens(['[X]', "[Y]"], special_tokens=True)
     emb = model.embeddings.weight.detach().numpy()
-    for f in os.listdir(args.input_dir):
-        filepath = os.path.join(args.input_dir, f, "prompt_vecs.npy")
+    for relation in os.listdir(args.input_dir):
+        filepath = os.path.join(args.input_dir, relation, "prompt_vecs.npy")
         with open(filepath, "rb") as f:
             prompt_vec = np.load(f)
         neigh = NearestNeighbors(n_neighbors=1)
@@ -26,12 +27,12 @@ if __name__ == "__main__":
         discrete_tokens = neigh.kneighbors(prompt_vec, 1, return_distance=False)
 
         discrete_tokens = np.squeeze(discrete_tokens)[:5]
-        prompt_string = model.tokenizer.decode(discrete_tokens)
-        prompt_string = "[X]{} [Y].".format(prompt_string)
         prompt_tokens = model.tokenizer.convert_ids_to_tokens(discrete_tokens)
+        prompt = ["[X]"] + prompt_tokens + ["[Y]"] + ["."]
+        prompt = model.tokenizer.convert_tokens_to_string(prompt)
         out = {
-            'relation': f,
-            'template': prompt_string,
+            'relation': relation,
+            'template': prompt,
             'tokens': prompt_tokens
         }
         out_json = json.dumps(out)
